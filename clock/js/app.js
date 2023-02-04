176fd61267da1d4446bbda99e202daedda232691
able808 <able.kodes@gmail.com>
2023-02-04 12:00:51 +0530

    fixed timing issues

--- clock/js/app.js
+++ clock/js/app.js
@@ -6,21 +6,21 @@ const hoursHand = document.querySelector('#hours')
 function setTime (){
     const now = new Date()
 
-   const seconds = now.getSeconds()
-   const secondsDeg = ((seconds/60)*360)+90
+    seconds = now.getSeconds()
+    secondsDeg = ((seconds/60)*360)+90
     secondsHand.style.transform = `rotate(${secondsDeg}deg)`
 
-   const minutes = now.getMinutes()
-   const minutesdeg =( (minutes/60)*360)+90
+    minutes = now.getMinutes()
+    minutesdeg =( (minutes/60)*360)+90
     minutesHand.style.transform = `rotate(${minutesdeg}deg)`
-    console.log(minutes, minutesdeg)
+    console.log(minutesdeg)
     
 
-    hours = now.getMinutes()
-    hoursdeg = ((minutes/12)*360)+90
-    minutesHand.style.transform = `rotate(${hoursdeg}deg)`
+    hours = now.getHours()
+    hoursdeg = ((hours/12)*360)+90
+    hoursHand.style.transform = `rotate(${hoursdeg}deg)`
     
-    console.log(now)
+    
 
 } 
 
